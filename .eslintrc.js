1c4757c990cab024af589339eba48ffcbaa5c5bc
Luis <luiscvlh11@gmail.com>
2022-03-24 10:28:22 -0300

    Start project

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/extensions': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vite';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import vue from '@vitejs/plugin-vue';
+
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+});
