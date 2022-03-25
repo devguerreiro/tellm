e93194196d6a0749f364a3d8b5d24a24e34b338d
Luis <luiscvlh11@gmail.com>
2022-03-24 21:29:52 -0300

    Added search animate icon

---
+++ src/components/AppSearching.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { SearchIcon } from '@heroicons/vue/solid';
+
+</script>
+
+<template>
+  <SearchIcon
+    class="w-10 h-10 text-yellow-600 search"
+  />
+</template>
+
+<style lang="scss">
+.search {
+  animation-name: searching;
+  animation-duration: 1s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+}
+
+@keyframes searching {
+  0% {
+    transform: translate(-2px, 2px);
+  }
+  25% {
+    transform: translate(-2px, -2px);
+  }
+  50% {
+    transform: translate(2px, -2px);
+  }
+  75% {
+    transform: translate(2px, 2px);
+  }
+  100% {
+    transform: translate(-2px, 2px);
+  }
+}
+</style>
--- src/pages/home/HomeIndex.vue
+++ src/pages/home/HomeIndex.vue
@@ -2,12 +2,24 @@
 import AppTitle from '@/components/AppTitle.vue';
 import AppButton from '@/components/AppButton.vue';
 import AppTask from '@/components/AppTask.vue';
+import AppSearching from '@/components/AppSearching.vue';
 
 import useTaskStore from '@/store/task';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
+const loading = ref(false);
 const store = useTaskStore();
 const hasTask = computed(() => Object.keys(store.task).length);
+
+async function handleGetTask(): Promise<void> {
+  try {
+    loading.value = true;
+    await store.getTask();
+    loading.value = false;
+  } catch (e) {
+    console.error(e);
+  }
+}
 </script>
 
 <template>
@@ -18,12 +30,16 @@ const hasTask = computed(() => Object.keys(store.task).length);
       </AppTitle>
     </header>
     <main class="mt-6">
-      <section>
-        <AppButton @click="store.getTask">
+      <section class="mx-auto">
+        <AppButton @click="handleGetTask">
           Give me an idea <span class="ml-2">🎁</span>
         </AppButton>
+        <AppSearching
+          v-if="loading"
+          class="mx-auto mt-4"
+        />
         <AppTask
-          v-if="hasTask"
+          v-else-if="!loading && hasTask"
           class="w-2/3 mx-auto mt-6 md:w-1/2 lg:w-1/4"
         />
       </section>
